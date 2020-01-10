462e9d5f6f8236a26cd00449238afc27d4eee484
Harley Salas <harley@harleysalas.com>
2020-01-10 05:31:02 +0300

    added chart component for history

---
+++ client/src/components/Chart/Chart.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Bar } from "react-chartjs-2";
+import "./Chart.scss";
+
+const Chart = props => {
+  const [data, setData] = useState({
+    labels: props.labels,
+    datasets: [
+      {
+        label: "Increase %",
+        data: props.data
+      }
+    ]
+  });
+
+  return (
+    <div className="chart">
+      <Bar
+        data={data}
+        options={{
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  fontFamily: "Raleway",
+                  fontSize: 18,
+                  fontColor: "#0e1113"
+                },
+                gridLines: {
+                  color: "#c5c5ce"
+                }
+              }
+            ],
+            xAxes: [
+              {
+                ticks: {
+                  fontFamily: "Raleway",
+                  fontSize: 18,
+                  fontWeight: 600,
+                  fontColor: "#0e1113",
+                  fontStyle: "600"
+                },
+                gridLines: {
+                  color: "#c5c5ce"
+                }
+              }
+            ]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            rectangle: {
+              backgroundColor: "#003eff"
+            }
+          },
+          animation: {
+            duration: 1440
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default Chart;
