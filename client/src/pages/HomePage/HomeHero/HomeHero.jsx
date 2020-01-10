c5139bbc1e1092752c8f1d37213c4b47a6ebe85e
Harley Salas <harley@harleysalas.com>
2020-01-10 03:13:31 +0300

    added home page

---
+++ client/src/pages/HomePage/HomeHero/HomeHero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomeHero = () => {
+  return (
+    <div className="home-hero">
+      <div className="container home-hero__container">
+        <h1 className="home-hero__heading">Share Your History</h1>
+        <span className="home-hero__subtitle">How much would your stock portfolio be worth today?</span>
+      </div>
+    </div>
+  );
+};
+
+export default HomeHero;
---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import HomeHero from "./HomeHero/HomeHero";
+
+const HomePage = () => {
+  return (
+    <div className="page">
+      <HomeHero />
+    </div>
+  );
+};
+
+export default HomePage;
