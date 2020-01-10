5479c26dd5ff38dcf6e87023a40697c2ed90802a
Harley Salas <harley@harleysalas.com>
2020-01-10 03:31:39 +0300

    finished simple homepage styling

--- client/src/pages/HomePage/HomeHero/HomeHero.jsx
+++ client/src/pages/HomePage/HomeHero/HomeHero.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import "./HomeHero.scss";
+
+import Button from "../../../components/Button/Button";
 
 const HomeHero = () => {
   return (
@@ -6,6 +9,7 @@ const HomeHero = () => {
       <div className="container home-hero__container">
         <h1 className="home-hero__heading">Share Your History</h1>
         <span className="home-hero__subtitle">How much would your stock portfolio be worth today?</span>
+        <Button btnText="Get Started" btnStyle="white" linkTo="/portfolio/name" />
       </div>
     </div>
   );
